850089c7311b8a2a0f8cf31170d2940887d824c7
Eliezer <eliezerassuncaocustodio@hotmail.com>
2023-02-07 12:47:40 -0300

    atualizacao nos cards

--- src/colors/index.js
+++
@@ -1,9 +0,0 @@
-export const colors = {
-  ligh_pink: '#f8d9c6',
-  white:'#fff',
-  thin_white:'#f4effa',
-  blue_purple: '#020314',
-  glossy_crape:'#9C89B8',
-  ash_gray:'#C2C5BB',
-  brow_sugar:'#B4654A'
-}
